b09ddad3e271508d7fe3137ee395a124771a694c
Scott Howell III <showell@PSM-W4353953.local>
2016-04-06 14:30:21 -0500

    Changed the init file for JBoss

--- files/jboss-as-standalone-Ubuntu.sh
+++ files/jboss-as-standalone-Ubuntu.sh
@@ -1,192 +1,41 @@
-#!/bin/bash
-# {{ ansible_managed }}
-#
-# JBoss standalone control script
-#
-# chkconfig: - 80 20
-# description: JBoss AS Standalone
-# processname: standalone
-# pidfile: /var/run/jboss-as/jboss-as-standalone.pid
-# config: /etc/jboss-as/jboss-as.conf
-
-# Define LSB log_* functions.
-# Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
-. /lib/lsb/init-functions
-
-# Load Java configuration.
-[ -r /etc/java/java.conf ] && . /etc/java/java.conf
-export JAVA_HOME
-
-##
-# Set the JBoss user
-JBOSS_USER=jboss  
-export JBOSS_USER 
-
-# Load JBoss AS init.d configuration.
-if [ -z "$JBOSS_CONF" ]; then
-  JBOSS_CONF="/etc/jboss-as/jboss-as.conf"
-fi
-
-[ -r "$JBOSS_CONF" ] && . "${JBOSS_CONF}"
-
-# Set defaults.
-
-if [ -z "$JBOSS_HOME" ]; then
-  JBOSS_HOME=/usr/share/jboss-as
-fi
-export JBOSS_HOME
-
-if [ -z "$JBOSS_PIDFILE" ]; then
-  JBOSS_PIDFILE=/var/run/jboss-as/jboss-as-standalone.pid
-fi
-export JBOSS_PIDFILE
-
-if [ -z "$JBOSS_CONSOLE_LOG" ]; then
-  JBOSS_CONSOLE_LOG=/var/log/jboss-as/console.log
-fi
-
-if [ -z "$STARTUP_WAIT" ]; then
-  STARTUP_WAIT=30
-fi
-
-if [ -z "$SHUTDOWN_WAIT" ]; then
-  SHUTDOWN_WAIT=30
-fi
-
-if [ -z "$JBOSS_CONFIG" ]; then
-  JBOSS_CONFIG=standalone.xml
-fi
-
-JBOSS_SCRIPT=$JBOSS_HOME/bin/standalone.sh
-
-prog='jboss-as'
-
-CMD_PREFIX=''
-
-if [ ! -z "$JBOSS_USER" ]; then
-  if [ -x /lib/lsb/init-functions ]; then
-    CMD_PREFIX="daemon --user $JBOSS_USER"
-  else
-    CMD_PREFIX="su - $JBOSS_USER -c"
-  fi
-fi
-
-success()
-{
-        echo -ne "[  OK  ]\r"
-}
-
-failure()
-{
-        echo -ne "[FAILED]\r"
-}
-
-start() {
-  echo -n "Starting $prog: "
-  if [ -f $JBOSS_PIDFILE ]; then
-    read ppid < $JBOSS_PIDFILE
-    if [ `ps --pid $ppid 2> /dev/null | grep -c $ppid 2> /dev/null` -eq '1' ]; then
-      echo -n "$prog is already running"
-      failure
-      echo
-      return 1 
-    else
-      rm -f $JBOSS_PIDFILE
-    fi
-  fi
-  mkdir -p $(dirname $JBOSS_CONSOLE_LOG)
-  cat /dev/null > $JBOSS_CONSOLE_LOG
-
-  mkdir -p $(dirname $JBOSS_PIDFILE)
-  chown $JBOSS_USER $(dirname $JBOSS_PIDFILE) || true
-  #$CMD_PREFIX JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT 2>&1 > $JBOSS_CONSOLE_LOG &
-  #$CMD_PREFIX JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT &
-
-  if [ ! -z "$JBOSS_USER" ]; then
-    if [ -x /lib/lsb/init-functions ]; then
-      daemon --user $JBOSS_USER LAUNCH_JBOSS_IN_BACKGROUND=1 JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT -c $JBOSS_CONFIG 2>&1 > $JBOSS_CONSOLE_LOG &
-    else
-      su - $JBOSS_USER -c "LAUNCH_JBOSS_IN_BACKGROUND=1 JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT -c $JBOSS_CONFIG" 2>&1 > $JBOSS_CONSOLE_LOG &
-    fi
-  fi
-
-  count=0
-  launched=false
-
-  until [ $count -gt $STARTUP_WAIT ]
-  do
-    grep 'JBoss AS.*started in' $JBOSS_CONSOLE_LOG > /dev/null 
-    if [ $? -eq 0 ] ; then
-      launched=true
-      break
-    fi 
-    sleep 1
-    let count=$count+1;
-  done
+#!/bin/bash  
+### BEGIN INIT INFO  
+# Provides:          jbossas7  
+# Required-Start:    $local_fs $remote_fs $network $syslog  
+# Required-Stop:     $local_fs $remote_fs $network $syslog  
+# Default-Start:     2 3 4 5  
+# Default-Stop:      0 1 6  
+# Short-Description: Start/Stop JBoss AS 7  
+### END INIT INFO  
+# chkconfig: 35 92 1  
   
-  success
-  echo
-  return 0
-}
-
-stop() {
-  echo -n $"Stopping $prog: "
-  count=0;
-
-  if [ -f $JBOSS_PIDFILE ]; then
-    read kpid < $JBOSS_PIDFILE
-    let kwait=$SHUTDOWN_WAIT
-
-    # Try issuing SIGTERM
-
-    kill -15 $kpid
-    until [ `ps --pid $kpid 2> /dev/null | grep -c $kpid 2> /dev/null` -eq '0' ] || [ $count -gt $kwait ]
-    do
-      sleep 1
-      let count=$count+1;
-    done
-
-    if [ $count -gt $kwait ]; then
-      kill -9 $kpid
-    fi
-  fi
-  rm -f $JBOSS_PIDFILE
-  success
-  echo
-}
-
-status() {
-  if [ -f $JBOSS_PIDFILE ]; then
-    read ppid < $JBOSS_PIDFILE
-    if [ `ps --pid $ppid 2> /dev/null | grep -c $ppid 2> /dev/null` -eq '1' ]; then
-      echo "$prog is running (pid $ppid)"
-      return 0
-    else
-      echo "$prog dead but pid file exists"
-      return 1
-    fi
-  fi
-  echo "$prog is not running"
-  return 3
-}
-
-case "$1" in
-  start)
-      start
-      ;;
-  stop)
-      stop
-      ;;
-  restart)
-      $0 stop
-      $0 start
-      ;;
-  status)
-      status
-      ;;
-  *)
-      ## If no parameters are given, print which are avaiable.
-      echo "Usage: $0 {start|stop|status|restart|reload}"
-      exit 1
-      ;;
-esac
+## Include some script files in order to set and export environmental variables  
+## as well as add the appropriate executables to $PATH.  
+[ -r /etc/profile.d/java.sh ] && . /etc/profile.d/java.sh  
+[ -r /etc/profile.d/jboss.sh ] && . /etc/profile.d/jboss.sh  
+  
+JBOSS_HOME=/usr/share/jboss-as 
+  
+AS7_OPTS="$AS7_OPTS -Dorg.apache.tomcat.util.http.ServerCookie.ALLOW_HTTP_SEPARATORS_IN_V0=true"   ## See AS7-1625  
+AS7_OPTS="$AS7_OPTS -Djboss.bind.address.management=0.0.0.0"  
+AS7_OPTS="$AS7_OPTS -Djboss.bind.address=0.0.0.0"  
+  
+case "$1" in  
+    start)  
+        echo "Starting JBoss AS 7..."  
+        #sudo -u jboss sh ${JBOSS_HOME}/bin/standalone.sh $AS7_OPTS           ##  If running as user "jboss"  
+        #start-stop-daemon --start --quiet --background --chuid jboss --exec ${JBOSS_HOME}/bin/standalone.sh -- $AS7_OPTS   ## Ubuntu  
+        ${JBOSS_HOME}/bin/standalone.sh $AS7_OPTS &  
+    ;;  
+    stop)  
+        echo "Stopping JBoss AS 7..."  
+        #sudo -u jboss sh ${JBOSS_HOME}/bin/jboss-admin.sh --connect command=:shutdown            ##  If running as user "jboss"  
+        #start-stop-daemon --start --quiet --background --chuid jboss --exec ${JBOSS_HOME}/bin/jboss-admin.sh -- --connect command=:shutdown     ## Ubuntu  
+        ${JBOSS_HOME}/bin/jboss-cli.sh --connect command=:shutdown  
+    ;;  
+    *)  
+        echo "Usage: /etc/init.d/jbossas7 {start|stop}"; exit 1;  
+    ;;  
+esac  
+  
+exit 0  
